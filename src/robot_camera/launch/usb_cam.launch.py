629f6398e204362e1a2886ad97a5610bb7d8884d
Jayson_Bui <doanh762003@gmail.com>
2025-12-06 15:14:07 +0700

    src

--- src/robot_camera/launch/usb_cam.launch.py
+++ src/robot_camera/launch/usb_cam.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
         executable='v4l2_camera_node',
         name='usb_camera',
         parameters=[
-            {"video_device": "/dev/video2"},
+            {"video_device": "/dev/video0"},
             {"image_size": [352, 288]},
             {"pixel_format": "YUYV"},
             {"frame_rate": 30},   # có thể sửa nếu muốn
